5200718bc5d47d67afb32d65afea780c65b30047
Suzanne-Pach <suzannepch@gmail.com>
2022-04-05 10:28:32 +0200

    Added styling feautered work section on homepage

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -25,24 +25,25 @@ get_header(); ?>
 
 <!-- FEATURED WORK -->
 <section class="featured-work">
-	<div class="site-content">
+	<div class="site-content clearfix">
 		<h4>Featured Work</h4>
 		<ul class="homepage-featured-work">
-			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
-				<?php while ( have_posts() ) : the_post(); 
-					$image_1 = get_field("image_1");
-					$size = "medium";
-				?>
-					<li class="individual-featured-work">
-						<figure>
-							<a  href="<?php the_permalink(); ?>" class="read-more-link">
-								<?php echo wp_get_attachment_image( $image_1, $size ); ?>
-							</a>
-						</figure>
-						<h3><a  href="<?php the_permalink(); ?>" class="read-more-link"><?php the_title(); ?></a></h3>
-					</li>
-				<?php endwhile; ?> 
-			<?php wp_reset_query(); ?>
+		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+		<!-- the loop -->
+			<?php while ( have_posts() ) : the_post(); 
+				$image_1 = get_field("image_1");
+				$size = "medium";
+			?>
+			<li class="individual-featured-work">
+				<a  href="<?php the_permalink(); ?>">
+					<figure>	
+						<?php echo wp_get_attachment_image( $image_1, $size ); ?>
+					</figure>
+					<h3><?php the_title(); ?></h3>
+				</a>
+			</li>
+			<?php endwhile; // end of the loop. ?> 
+		<?php wp_reset_query(); // resets the altered query back to the original ?>
 		</ul>
 	</div>
 </section>
