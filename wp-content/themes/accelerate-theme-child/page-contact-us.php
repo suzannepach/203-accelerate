bd5bef59dafb9198038fd0a197b4a0ccbe1ec201
Suzanne-Pach <suzannepch@gmail.com>
2022-04-12 14:46:08 +0200

    Made style changes to the contact form

--- wp-content/themes/accelerate-theme-child/page-contact-us.php
+++ wp-content/themes/accelerate-theme-child/page-contact-us.php
@@ -1,11 +1,8 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying contact pages
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * This is the template that displays contact pages by default.
  *
  * @package WordPress
  * @subpackage Accelerate Marketing
@@ -17,7 +14,7 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<h2><?php the_title(); ?></h2>
+				<h1><?php the_title(); ?></h1>
 				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
