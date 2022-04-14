5c2e3de74ec8432ff360f65e33e8fbc0d2831238
Suzanne-Pach <suzannepch@gmail.com>
2022-04-14 14:27:33 +0200

    Added styles to footer

--- wp-content/themes/accelerate-theme/footer.php
+++ wp-content/themes/accelerate-theme/footer.php
@@ -14,14 +14,15 @@
 
 		<footer id="colophon" class="site-footer clearfix" role="contentinfo">
 			<div class="site-info">
+				<!-- I have moved the social menu above the site description so it looks better on mobile.
+				On bigger screen I have to use flex styles to make sure the icons are positioned om teh right not on the left. -->
+				<nav class="social-media-navigation" role="navigation">
+					<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu' ) ); ?>
+				</nav>
 				<div class="site-description">
 					<p><?php bloginfo('description'); ?></p>
 					<p>&copy; <?php bloginfo('title'); ?>, LLC
 				</div>
-
-				<nav class="social-media-navigation" role="navigation">
-					<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu' ) ); ?>
-				</nav>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 
